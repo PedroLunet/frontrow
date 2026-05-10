ab7daa00a966b174d0015d994a88708f69730749
Pedro Lunet <pedrolunet@gmail.com>
2026-05-10 21:23:25 +0100

    added map component with costum mapbox style

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import mapboxgl from 'mapbox-gl';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public';
+
+	let { styleUrl = 'mapbox://styles/plunet/cmp06gq6u005a01s338r3aszl' } = $props();
+
+	let mapContainer: HTMLElement;
+	let map: mapboxgl.Map;
+
+	onMount(() => {
+		mapboxgl.accessToken = PUBLIC_MAPBOX_TOKEN;
+
+		map = new mapboxgl.Map({
+			container: mapContainer,
+			style: styleUrl,
+			center: [-8.6109, 41.1496],
+			zoom: 17,
+			pitch: 70
+		});
+
+		return () => {
+			if (map) map.remove();
+		};
+	});
+</script>
+
+<div bind:this={mapContainer} class="h-full w-full rounded-xl shadow-inner"></div>
